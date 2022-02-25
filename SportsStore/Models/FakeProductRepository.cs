3741a6fdb330077b4d6fe717d86ed0b19145a84f
eugene <bykov.ey@gmail.com>
2022-02-25 17:58:01 +1000

    Добавлен генератор данных

---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>()
+        {
+            new Product { Id = 1, Name = "Footbal", Price = 20},
+            new Product { Id = 2, Name = "Surf board", Price = 12},
+            new Product { Id = 3, Name = "Running shoes", Price = 23}
+        }.AsQueryable<Product>();
+    }
+}
